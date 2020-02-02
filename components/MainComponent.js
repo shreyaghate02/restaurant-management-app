b302113ade22e7b568abcfe8291bef698a3ff35b
udshreyag <shreya.ghate@udgama.com>
2020-02-02 20:50:32 +0530

    Navigation part 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,33 +1,68 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator,createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 const MenuNavigator = createStackNavigator({
-        Menu: { screen: Menu },
-        Dishdetail: { screen: Dishdetail }
-    },
-    {
-        initialRouteName: 'Menu',
-        navigationOptions: {
-            headerStyle: {
-                backgroundColor: "#512DA8"
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: "#fff"
-            }
-        }
-    }
+  Menu: { screen: Menu },
+  Dishdetail: { screen: Dishdetail }
+},
+{
+  initialRouteName: 'Menu',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: "#fff"
+      }
+  }
+}
 );
+
+const HomeNavigator = createStackNavigator({
+Home: { screen: Home }
+}, {
+navigationOptions: ({ navigation }) => ({
+headerStyle: {
+    backgroundColor: "#512DA8"
+},
+headerTitleStyle: {
+    color: "#fff"
+},
+headerTintColor: "#fff"
+})
+});
+
+const MainNavigator = createDrawerNavigator({
+Home:
+{ screen: HomeNavigator,
+  navigationOptions: {
+    title: 'Home',
+    drawerLabel: 'Home'
+  }
+},
+Menu:
+{ screen: MenuNavigator,
+  navigationOptions: {
+    title: 'Menu',
+    drawerLabel: 'Menu'
+  },
+}
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
+
 
 class Main extends Component {
   render() {
     return (
         <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MenuNavigator />
+            <MainNavigator />
         </View>
     );
   }
