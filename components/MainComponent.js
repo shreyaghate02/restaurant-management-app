7958e8dd3529a709f23581195f36d15e68182f8b
udshreyag <shreya.ghate@udgama.com>
2020-03-05 22:21:27 +0530

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,8 @@ import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
+
 
 
 const mapStateToProps = state => {
@@ -85,6 +87,23 @@ const ReservationNavigator = createStackNavigator({
       onPress={ () => navigation.navigate('DrawerToggle') } />    
   })
 });
+
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.navigate('DrawerToggle') } />    
+  })
+})
 
 const HomeNavigator = createStackNavigator({
     Home: { screen: Home }
@@ -204,6 +223,23 @@ const MainNavigator = createDrawerNavigator({
             )
           },
         },
+
+        Favorites:
+        { screen: FavoritesNavigator,
+          navigationOptions: {
+            title: 'My Favorites',
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({ tintColor, focused }) => (
+              <Icon
+                name='heart'
+                type='font-awesome'            
+                size={24}
+                iconStyle={{ color: tintColor }}
+              />
+            ),
+          }
+        },
+
         Reservation:
         { screen: ReservationNavigator,
           navigationOptions: {
