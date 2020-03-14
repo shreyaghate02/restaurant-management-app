e57d0066ce7eadbaeeaa043de80006d3ab5a522f
udshreyag <shreya.ghate@udgama.com>
2020-03-14 12:01:54 +0530

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -11,7 +11,8 @@ import {
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
-
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
   static navigationOptions = {
@@ -53,12 +54,42 @@ class Reservation extends Component {
         },
         {
           text: 'OK',
-          onPress: () => this.confirmReservation(),
+          onPress: () => {
+            this.presentLocalNotification(this.state.date);
+            this.confirmReservation();
+          }
         },
       ],
       { cancelable: false },
     );
   }
+
+  async obtainNotificationPermission() {
+    let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+    if (permission.status !== 'granted') {
+        permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            Alert.alert('Permission not granted to show notifications');
+        }
+    }
+    return permission;
+}
+
+async presentLocalNotification(date) {
+    await this.obtainNotificationPermission();
+    Notifications.presentLocalNotificationAsync({
+        title: 'Your Reservation',
+        body: 'Reservation for '+ date + ' requested',
+        ios: {
+            sound: true
+        },
+        android: {
+            sound: true,
+            vibrate: true,
+            color: '#512DA8'
+        }
+    });
+}
 
   render() {
     const todayDate = new Date().toISOString().split('T')[0];
