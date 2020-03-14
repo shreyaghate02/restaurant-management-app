6ed1c860e22af546d9b0e5e51a60b93d0b708c6a
udshreyag <shreya.ghate@udgama.com>
2020-03-14 20:26:34 +0530

    Sending Email

--- components/ContactUsComponent.js
+++ components/ContactUsComponent.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
-import { Card } from 'react-native-elements';
+import {View, FlatList, Text, StyleSheet} from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class ContactUs extends Component {
+  sendMail() {
+    MailComposer.composeAsync({
+        recipients: ['confusion@food.net'],
+        subject: 'Enquiry',
+        body: 'To whom it may concern:'
+    })
+}
+constructor(props){
+  super(props);
+}
+
+static navigationOptions = {
+    title: 'Contact Us'
+};
 
 render(){
   return(
-    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
-                <Card
-                    title='Contact Information'>
-    <Text>121, Clear Water Bay Road</Text>
-            <Text>Clear Water Bay, Kowloon</Text>
-            <Text>HONG KONG  </Text>
-            <Text>Tel: +852 1234 5678</Text>
-            <Text>Fax: +852 8765 4321</Text>
-            <Text>Email:confusion@food.net</Text>
-            </Card>
-                    </Animatable.View>
-)
+    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+      <Card
+          title='Contact Information' >
+        <View>
+          <FlatList
+              data={[
+                {key:'121, Clear Water Bay Road'},
+                {key:'Clear Water Bay, Kowloon'},
+                {key:'HONG KONG'},
+                {key:'Tel: +852 1234 5678'},
+                {key:'Fax: +852 8765 4321'},
+                {key:'Email:confusion@food.net'},
+              ]}
+            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+            />
+            <Button
+              title="Send Email"
+              buttonStyle={{backgroundColor: "#512DA8"}}
+              icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+              onPress={this.sendMail}
+            />
+        </View>
+      </Card>
+      </Animatable.View>
+  );
 }
 }
+
+const styles = StyleSheet.create({
+item: {
+  padding: 10,
+  fontSize: 16,
+  height: 44,
+},
+})
+
 export default ContactUs;
